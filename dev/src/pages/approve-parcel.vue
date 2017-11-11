39766c8d04d07562f91418c12969d7cffd7c9453
ALYONA\Alyona <zaa145@yandex.ru>
2017-11-11 03:08:03 +0300

    some changes in css

--- dev/src/pages/approve-parcel.vue
+++ dev/src/pages/approve-parcel.vue
@@ -9,14 +9,13 @@
                     Approve
                 </f7-nav-left>
             </f7-navbar>
-            <i class="material-icons icon-style" style="color: rgb(96, 191, 132);" id="approveicon" >verified_user</i>
+            <i class="material-icons icon-style" style="color: rgb(96, 191, 132);" id="approveicon">verified_user</i>
             <f7-block-title class="title-style">Confirm receipt of the {{name}}</f7-block-title>
-            <i class="material-icons icon-style"  id="trash" @click="clear()">replay</i>
+            <i class="material-icons icon-style" id="trash" @click="clear()">replay</i>
 
             <f7-block>
-
                 <div id="watermark">Please, sign here</div>
-                    <vueSignature ref="signature" :sigOption="sigConfig"></vueSignature>
+                <vueSignature ref="signature" :sigOption="sigConfig"></vueSignature>
                 <f7-buttons>
                     <f7-button fill class="buttonConfirm" @click="save()" style="margin-top:16px">
                         Confirm
@@ -45,37 +44,39 @@
         },
 
 
-		methods: {
-			save() {
-				var _this = this;
-				var png = _this.$refs.signature.save()
-				var jpeg = _this.$refs.signature.save('image/jpeg')
-				var svg = _this.$refs.signature.save('image/svg+xml');
-				console.log(png);
-				console.log(jpeg)
-				console.log(svg)
+        methods: {
+            save() {
 
-			},
-			clear() {
-				var _this = this;
-				_this.$refs.signature.clear();
-			},
-			resizeCanvas() {
-				var canvas = document.getElementById("canvas");
-				canvas.width  =innerWidth;
-				canvas.height = 280;
-                canvas.style.background='white';
-                canvas.style.border='4px dashed rgba(0, 0, 0, .2)';
-                canvas.style.paddingLeft='0';
-                canvas.style.paddingRight='0';
-                canvas.style.marginLeft='auto';
-                canvas.style.marginRight='auto';
-                canvas.style.display='block';
-                canvas.style.width='-webkit-fill-available';
-			}
-		},
-		mounted: function () {
-			this.resizeCanvas()
-		}
-	};
+                var _this = this;
+
+                var png = _this.$refs.signature.save()
+                var jpeg = _this.$refs.signature.save('image/jpeg')
+                var svg = _this.$refs.signature.save('image/svg+xml');
+                console.log(png);
+                console.log(jpeg)
+                console.log(svg)
+
+            },
+            clear() {
+                var _this = this;
+                _this.$refs.signature.clear();
+            },
+            resizeCanvas() {
+                var canvas = document.getElementById("canvas");
+                canvas.width = innerWidth;
+                canvas.height = 280;
+                canvas.style.background = 'white';
+                canvas.style.border = '4px dashed rgba(0, 0, 0, .2)';
+                canvas.style.paddingLeft = '0';
+                canvas.style.paddingRight = '0';
+                canvas.style.marginLeft = 'auto';
+                canvas.style.marginRight = 'auto';
+                canvas.style.display = 'block';
+                canvas.style.width = '-webkit-fill-available';
+            }
+        },
+        mounted: function () {
+            this.resizeCanvas()
+        }
+    };
 </script>
